9cea7de7259083d70f34e47e6e4dbd88f9279f21
Robson Leite <robson.leite@rapiddo.com.br>
2017-08-16 15:21:21 -0300

    start project commit.

---
+++ src/main/java/br/com/integralabs/oficina/controller/GenericController.java
@@ -0,0 +1,13 @@
+package br.com.integralabs.oficina.controller;
+
+import br.com.integralabs.oficina.utils.AppConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by robson on 16/08/17.
+ */
+public class GenericController {
+
+    protected static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+}
---
+++ src/main/java/br/com/integralabs/oficina/utils/OficinaConstant.java
@@ -0,0 +1,9 @@
+package br.com.integralabs.oficina.utils;
+
+/**
+ * Created by robson on 16/08/17.
+ */
+public class OficinaConstant {
+
+    public static final String API_TOKEN = "apiToken";
+}
