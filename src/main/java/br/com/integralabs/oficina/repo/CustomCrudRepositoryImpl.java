97dc88c694de9afc801d64dbacbbb3dca11cce8c
Robson Leite <robsoncarvalholeite@gmail.com>
2017-09-03 20:13:36 -0300

    Change CustomCrudRepository.

--- src/main/java/br/com/integralabs/oficina/repo/CustomCrudRepositoryImpl.java
+++ src/main/java/br/com/integralabs/oficina/repo/CustomCrudRepositoryImpl.java
@@ -1,11 +1,19 @@
-package br.com.integralabs.oficina.test;
+package br.com.integralabs.oficina.repo;
 
 import br.com.integralabs.oficina.model.BaseModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.lang.reflect.ParameterizedType;
+import javax.transaction.Transactional;
+import java.beans.FeatureDescriptor;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by robson on 03/09/17.
@@ -17,12 +25,39 @@ public class CustomCrudRepositoryImpl<T extends BaseModel, ID extends Long> impl
     private EntityManager em;
 
     @Override
+    @Transactional(Transactional.TxType.REQUIRED)
     public T merge(T model) {
-        return em.merge(model);
+        return this.em.merge(model);
     }
 
     @Override
-    public T findFirst(ID id, Class<T> clazz) {
-        return em.find(clazz, id);
+    @Transactional(Transactional.TxType.REQUIRED)
+    public T updateNonNullFields(T model) {
+        T baseModelFromDB = this.em.find((Class<T>) model.getClass(), model.getId());
+
+        if (Objects.isNull(baseModelFromDB))
+            return null;
+
+        this.mergeNonNullField(baseModelFromDB, model);
+
+        return this.merge(baseModelFromDB);
+    }
+
+    /**
+     * Copy All non null fields from source Entity to target entity.
+     *
+     * @param target to Object fields.
+     * @param source from Object fields.
+     */
+    private void mergeNonNullField(T target, T source) {
+
+        BeanWrapper beanWrapper = new BeanWrapperImpl(source);
+        List<String> nullFieldsName = Arrays.asList(beanWrapper.getPropertyDescriptors())
+                .stream()
+                .map(FeatureDescriptor::getName)
+                .filter(fieldName -> (Objects.isNull(beanWrapper.getPropertyValue(fieldName))))
+                .collect(Collectors.toList());
+
+        BeanUtils.copyProperties(source, target, nullFieldsName.toArray(new String[]{}));
     }
 }
--- src/main/java/br/com/integralabs/oficina/repo/GenericCrudRepository.java
+++ src/main/java/br/com/integralabs/oficina/repo/GenericCrudRepository.java
@@ -2,12 +2,11 @@ package br.com.integralabs.oficina.repo;
 
 import br.com.integralabs.oficina.model.BaseModel;
 import br.com.integralabs.oficina.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-
-import java.io.Serializable;
 
 /**
  * Created by a.moreira.rodrigues on 20/08/2017.
  */
-public interface BaseCrudRepository<T extends BaseModel, ID extends Long> extends CrudRepository<T, ID>, CustomCrudRepository<T, ID> {
+public interface GenericCrudRepository<T extends BaseModel> extends CrudRepository<T, Long>, CustomCrudRepository<T, Long> {
 }
