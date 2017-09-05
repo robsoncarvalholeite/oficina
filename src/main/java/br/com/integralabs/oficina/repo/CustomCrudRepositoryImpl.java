5956d930ae193667c31bf5ddce3a2cd4394fdaf5
Robson Leite <robsoncarvalholeite@gmail.com>
2017-09-05 00:47:51 -0300

    Added h2 dir on gitignore file.

--- src/main/java/br/com/integralabs/oficina/repo/CustomCrudRepositoryImpl.java
+++ src/main/java/br/com/integralabs/oficina/repo/CustomCrudRepositoryImpl.java
@@ -1,19 +1,13 @@
 package br.com.integralabs.oficina.repo;
 
 import br.com.integralabs.oficina.model.BaseModel;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
+import br.com.integralabs.oficina.utils.CloneObjectUtils;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.beans.FeatureDescriptor;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 /**
  * Created by robson on 03/09/17.
@@ -38,26 +32,9 @@ public class CustomCrudRepositoryImpl<T extends BaseModel, ID extends Long> impl
         if (Objects.isNull(baseModelFromDB))
             return null;
 
-        this.mergeNonNullField(baseModelFromDB, model);
+        // Apply only filled fields in clone.
+        CloneObjectUtils.mergeNonNullFields(baseModelFromDB, model);
 
         return this.merge(baseModelFromDB);
-    }
-
-    /**
-     * Copy All non null fields from source Entity to target entity.
-     *
-     * @param target to Object fields.
-     * @param source from Object fields.
-     */
-    private void mergeNonNullField(T target, T source) {
-
-        BeanWrapper beanWrapper = new BeanWrapperImpl(source);
-        List<String> nullFieldsName = Arrays.asList(beanWrapper.getPropertyDescriptors())
-                .stream()
-                .map(FeatureDescriptor::getName)
-                .filter(fieldName -> (Objects.isNull(beanWrapper.getPropertyValue(fieldName))))
-                .collect(Collectors.toList());
-
-        BeanUtils.copyProperties(source, target, nullFieldsName.toArray(new String[]{}));
     }
 }
